9bde139fa45be0b3a20394eada2185ac402a1802
voxels <kps.shelva@gmail.com>
2026-06-05 13:04:16 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
 const CACHE_NAME = "hello-pwa-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./icon-192.png",
-  "./icon-512.png"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
